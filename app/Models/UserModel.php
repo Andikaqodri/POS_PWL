2a03d6b769314b3d7cfc3289834a8e3d4069bf5b
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-09-28 23:19:36 +0700

    pertemuan 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -13,4 +12,5 @@ class UserModel extends Model
     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan UserModel
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
 
+    protected $fillable = ['level_id', 'username', 'nama' ];
 }
