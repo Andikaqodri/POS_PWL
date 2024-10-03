fbc4c44d71a32472bbdd65581b570c9a336687c0
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-03 21:49:46 +0700

    pertemuan 5

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class LevelModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'm_level';
-    protected $primarykey = 'level_id';
+    protected $primaryKey = 'level_id';
 
 
-    public function user ():BelongsTo {
+    public function level(): BelongsTo {
         return $this->belongsTo(UserModel::class);
     }
+
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,19 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan UserModel
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password' ];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    public function level(): BelongsTo {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
 }
