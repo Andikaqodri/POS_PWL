6c44c0b084a1933bfa6bc43f1758b4b7ea55a866
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-08 14:56:32 +0700

    pertemuan 7 praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,24 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-use function Laravel\Prompts\password;
+// class UserModel extends Model
+// {
+//     use HasFactory;
+//     protected $table = 'm_user';
+//     protected $primaryKey = 'user_id';
 
-class UserModel extends Model
-{
+//     // JOBSHEET 4
+//     // prak 1
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+//     // protected $fillable = ['level_id', 'username', 'nama'];
+
+//     // prak 2.4
+//     // protected $table = 'user';
+
+//     // prak 2.7
+//     public function level(): BelongsTo{
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+// }
+
+class UserModel extends Authenticatable{
     use HasFactory;
-
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    /**
-     * The attributes that are mass assignable
-     * 
-     * @var array
-     */
-    protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'update_at'];
+    protected $casts = ['password' => 'hashed'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    public function level(): BelongsTo{
+        return $this -> belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    public function getRoleName(): string{
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool{
+        return $this ->level->level_kode == $role;
+    }
+
+    public function getRole(){
+        return $this->level->level_kode;
+}
 }
