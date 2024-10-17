6135215e8db91554932c9dd48d707d03ded172c6
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-17 23:33:32 +0700

    pertemuan 8 praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,9 @@ Route::middleware(['auth', 'authorize:ADM, MNG'])->group(function () {
       Route::delete('/barang{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
       Route::get('/barang/import', [BarangController::class, 'import']);
       Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']);
+      Route::get('/import', [BarangController::class, 'import']); //uppload excel
+      Route::post('/import_ajax', [BarangController::class, 'import_ajax']); //ajax import excel
+      Route::get('/export_excel', [BarangController::class, 'export_excel']);
 
     });
 });
