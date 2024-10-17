a0247e6ad96411ba51ec8432ca038b930b55707a
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-17 23:20:21 +0700

    pertemuan 7 praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,30 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\BarangController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/', [WelcomeController::class, 'index']);
+
+
 Route::pattern('id', '[0-9]+');
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
@@ -26,7 +32,7 @@ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 Route::get('register', [AuthController::class, 'register'])->name('register');
 Route::post('register', [AuthController::class, 'postRegister']);
 
-Route::middleware(['authorize:ADM'])->group(function () {
+Route::middleware(['auth', 'authorize:ADM, MNG'])->group(function () {
 
     Route::get('/', [WelcomeController::class, 'index']); //halaman awal
 
@@ -96,19 +102,16 @@ Route::middleware(['authorize:ADM'])->group(function () {
         Route::delete('/{id}', [SupplierController::class, 'destroy']); //hapus data user
     });
     Route::group(['prefix' => 'barang'], function () {
-        Route::get('/', [BarangController::class, 'index']); //halaman awal
-        Route::post('/list', [BarangController::class, 'list']);  //data user (json)
-        Route::get('/create', [BarangController::class, 'create']); //form tambah user
-        Route::post('/', [BarangController::class, 'store']); //data user baru
-        Route::get('/create_ajax', [BarangController::class, 'create_ajax']); //form tambah user ajax
-        Route::post('/ajax', [BarangController::class, 'store_ajax']); //simpan data user baru ajax
-        Route::get('/{id}', [BarangController::class, 'show']); //detail user
-        Route::get('/{id}/edit', [BarangController::class, 'edit']); //form edit
-        Route::put('/{id}', [BarangController::class, 'update']); // simpan perubahan data
-        Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']); //tampilkan form edit dengan ajax
-        Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']); //simpan perubahan user ajax
-        Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); //confirm delete ajax
-        Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); //hapus ajax
-        Route::delete('/{id}', [BarangController::class, 'destroy']); //hapus data user
-});
+      Route::get('/barang',[BarangController::class,'index']);
+      Route::post('/barang/list',[BarangController::class,'list']);
+      Route::get('/barang/create_ajax',[BarangController::class, 'create_ajax']);
+      Route::post('/barang_ajax', [BarangController::class, 'store_ajax']);
+      Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+      Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+      Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+      Route::delete('/barang{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+      Route::get('/barang/import', [BarangController::class, 'import']);
+      Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']);
+
+    });
 });
