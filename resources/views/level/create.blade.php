3596e1dee5b0ac76691e93bc900771bab5492a72
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-18 11:09:41 +0700

    pertemuan 8 revisi

--- resources/views/level/create.blade.php
+++ resources/views/level/create.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.template')
-
 @section('content')
 <div class="card card-outline card-primary">
     <div class="card-header">
@@ -10,28 +9,25 @@
         <form method="POST" action="{{ url('level') }}" class="form-horizontal">
             @csrf
             <div class="form-group row">
-                <label class="col-1 control-label col-form-label">Kode Level</label>
-                <div class="col-11">
+                <label class="col-2 control-label col-form-label">Kode Level</label>
+                <div class="col-10">
                     <input type="text" class="form-control" id="level_kode" name="level_kode" value="{{ old('level_kode') }}" required>
                     @error('level_kode')
                         <small class="form-text text-danger">{{ $message }}</small>
                     @enderror
                 </div>
             </div>
-
             <div class="form-group row">
-                <label class="col-1 control-label col-form-label">Nama Level</label>
-                <div class="col-11">
+                <label class="col-2 control-label col-form-label">Nama Level</label>
+                <div class="col-10">
                     <input type="text" class="form-control" id="level_nama" name="level_nama" value="{{ old('level_nama') }}" required>
                     @error('level_nama')
                         <small class="form-text text-danger">{{ $message }}</small>
                     @enderror
                 </div>
             </div>
-
             <div class="form-group row">
-                <label class="col-1 control-label col-form-label"></label>
-                <div class="col-11">
+                <div class="col-10 offset-2">
                     <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
                     <a class="btn btn-sm btn-default ml-1" href="{{ url('level') }}">Kembali</a>
                 </div>
