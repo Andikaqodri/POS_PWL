4a2e65316b3bf84357141b8bd76ecfc4b53702e2
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-10-18 11:08:49 +0700

    pertemuan 8 revisi

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,6 +4,22 @@
     <title>Data User</title>
 </head>
 <body>
+    {{-- <h1>Data User</h1> --}}
+    {{-- <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{$data->user_id}} </td>
+            <td>{{$data->username}} </td>
+            <td>{{$data->nama}} </td>
+            <td>{{$data->level_id}} </td>
+        </tr>
+    </table> --}}
+
     <h1>Data User</h1>
     <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
@@ -30,23 +46,7 @@
         @endforeach
     
     </table>
+    
+
 </body>
 </html>
-
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $jumlah_pengguna }}</td>
-        </tr>
-    </table>
-</body>
-</html> --}}
